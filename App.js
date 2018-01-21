f2a9b02e0357400eec89c1310e266dc521a36ea9
Nethanel Kohen <nethanel.kohen@gmail.com>
2018-01-21 09:01:19 -0500

    npm update, barcode fix

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import BarcodeScanner from './components/BarcodeScanner.js';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {/* <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Shake your phone to open the developer menu.</Text> */}
+        <BarcodeScanner />
       </View>
     );
   }
@@ -18,6 +20,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
